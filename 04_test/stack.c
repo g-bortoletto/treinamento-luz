0571b70f5c98aecc4cc75ed901335f213ba05724
Guilherme Bortoletto <guilherme.mbortoletto@gmail.com>
2022-03-10 23:17:15 -0300

    Improve stack data structure

--- 04_test/stack.c
+++ 04_test/stack.c
@@ -1,129 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct stack_data_t
+typedef struct stack_t
 {
   int top;
   unsigned size;
   unsigned capacity;
-} stack_data_t;
+  int* data;
+} stack_t;
 
-static int* StackCreate(stack_data_t* stackData, unsigned capacity)
+static stack_t* StackCreate(unsigned capacity)
 {
-  int* stack = 0;
+  stack_t* stack = 0;
+  stack = (stack_t*)malloc(3 * sizeof(int) + sizeof(int*));
 
-  stackData->top = -1;
-  stackData->size = 0;
-  stackData->capacity = capacity;
-
-  stack = (int*)malloc(stackData->capacity * sizeof(int));
-  memset(stack, 0, sizeof(stack));
+  stack->top = -1;
+  stack->size = 0;
+  stack->capacity = capacity;
+  stack->data = (int*)malloc(stack->capacity * sizeof(int));
+  memset(stack->data, 0, sizeof(stack->data));
 
   return stack;
 }
 
-static void StackDestroy(stack_data_t* stackData, int* stack)
+static void StackDestroy(stack_t* stack)
 {
-  stackData->top = -1;
-  stackData->size = 0;
-  stackData->capacity = 0;
+  stack->top = -1;
+  stack->size = 0;
+  stack->capacity = 0;
 
-  free(stack);
+  free(stack->data = 0);
+  free(stack = 0);
 }
 
-static int StackPush(stack_data_t* stackData, int* stack, int element)
+static int StackPush(stack_t* stack, int element)
 {
   int result = 0;
 
-  if (stackData->size == stackData->capacity)
+  if (stack->size == stack->capacity)
   {
     result = 0;
   }
   else
   {
-    ++stackData->top;
-    stack[stackData->top] = element;
-    ++stackData->size;
+    ++stack->top;
+    stack->data[stack->top] = element;
+    ++stack->size;
     result = 1;
   }
 
   return result;
 }
 
-static int StackPop(stack_data_t* stackData, int* stack)
+static int StackPop(stack_t* stack)
 {
   int result = 0;
 
-  if (stackData->top == -1 || stackData->size == 0 || stackData->capacity == 0)
+  if (stack->top == -1 || stack->size == 0 || stack->capacity == 0)
   {
     result = 0;
   }
   else
   {
-    result = stack[stackData->top];
-    --stackData->top;
-    --stackData->size;
+    result = stack->data[stack->top];
+    stack->data[stack->top] = 0;
+    --stack->top;
+    --stack->size;
   }
 
   return result;
 }
 
-static int StackTop(stack_data_t* stackData, int* stack)
+static int StackTop(stack_t* stack)
 {
-  return stack[stackData->top];
+  return stack->data[stack->top];
 }
 
-static int StackIsEmpty(stack_data_t* stackData)
+static int StackIsEmpty(stack_t* stack)
 {
-  return stackData->size == 0;
+  return stack->size == 0;
 }
 
-static void StackPrint(stack_data_t* stackData, int* stack)
+static void StackPrint(stack_t* stack)
 {
   int i;
-  for (i = 0; i < stackData->size; ++i)
+  for (i = 0; i < stack->size; ++i)
   {
-    printf("%d ", stack[i]);
+    printf("%d ", stack->data[i]);
   }
   printf("\n");
 }
 
 int main()
 {
-  stack_data_t stackData;
-  int* stack = StackCreate(&stackData, 10);
+  stack_t* stack;
+  stack = StackCreate(10);
 
-  StackPush(&stackData, stack, 11);
-  StackPush(&stackData, stack, 22);
-  StackPush(&stackData, stack, 33);
-  StackPush(&stackData, stack, 44);
-  StackPush(&stackData, stack, 55);
-  StackPush(&stackData, stack, 66);
+  StackPush(stack, 11);
+  StackPush(stack, 22);
+  StackPush(stack, 33);
+  StackPush(stack, 44);
+  StackPush(stack, 55);
+  StackPush(stack, 66);
 
-  StackPrint(&stackData, stack);
+  StackPrint(stack);
 
   int popped = 0;
-  popped = StackPop(&stackData, stack);
+  popped = StackPop(stack);
   printf("Popped: %d\n", popped);
-  popped = StackPop(&stackData, stack);
+  popped = StackPop(stack);
   printf("Popped: %d\n", popped);
-  popped = StackPop(&stackData, stack);
-  printf("Popped: %d\n", popped);
-
-  StackPrint(&stackData, stack);
-
-  printf("Stack is empty: %s\n", StackIsEmpty(&stackData) ? "YES" : "NO");
-  printf("Stack top is: %d\n", StackTop(&stackData, stack));
-
-  popped = StackPop(&stackData, stack);
-  printf("Popped: %d\n", popped);
-  popped = StackPop(&stackData, stack);
-  printf("Popped: %d\n", popped);
-  popped = StackPop(&stackData, stack);
+  popped = StackPop(stack);
   printf("Popped: %d\n", popped);
 
-  printf("Stack is empty: %s\n", StackIsEmpty(&stackData) ? "YES" : "NO");
+  StackPrint(stack);
 
-  StackDestroy(&stackData, stack);
+  printf("Stack is empty: %s\n", StackIsEmpty(stack) ? "YES" : "NO");
+  printf("Stack top is: %d\n", StackTop(stack));
+
+  popped = StackPop(stack);
+  printf("Popped: %d\n", popped);
+  popped = StackPop(stack);
+  printf("Popped: %d\n", popped);
+  popped = StackPop(stack);
+  printf("Popped: %d\n", popped);
+
+  printf("Stack is empty: %s\n", StackIsEmpty(stack) ? "YES" : "NO");
+
+  StackDestroy(stack);
   return 0;
 }
